dd959bd4c0aadba5a2b016ab94516e702bcccc38
Leo Lo <leolo0626@gmail.com>
2021-08-04 13:53:55 +0800

    add buy signal library

--- src/DataLibrary.py
+++ src/DataLibrary.py
@@ -1,10 +1,16 @@
 class DataLibrary :
     def __init__(self):
         self.sell_signal_library = {}
+        self.buy_signal_library = {}
         self.stock_price_library = {}
-    
+        
     def add_stock_to_sell_signal_library(self, ticker, sell_signal_data):
         self.sell_signal_library[ticker] = sell_signal_data
     
+    def add_stock_to_buy_signal_library(self, ticker, buy_signal_data) :
+        self.buy_signal_library[ticker] = buy_signal_data
+    
     def add_stock_price_library(self, ticker, stock_price_data):
         self.stock_price_library[ticker] = stock_price_data
+    
+    
