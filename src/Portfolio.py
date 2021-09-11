5a27f3b535f45cedb66d2ba6600372b5fd3d7295
Leo Lo <leolo@Leos-Mac-mini.local>
2021-09-11 22:50:26 +0800

    add threading to improve speed

--- src/Portfolio.py
+++ src/Portfolio.py
@@ -5,7 +5,7 @@ from .DataLibrary import DataLibrary
 from .TradeRecordManager import TradeRecordManager
 from .AccountSummary import AccountSummary
 from .ErrorManager import ErrorManager
-import multiprocessing
+from multiprocessing.pool import ThreadPool
 import copy
 
 class Portfolio(Account) :
@@ -60,34 +60,16 @@ class Portfolio(Account) :
             raise Exception("Something went wrong in cover position ")
 
     def update_account_summary(self, date_time , data_library : DataLibrary, location='US') :
-
-        net_asset_value = 0
         positions = self.positions
-
-
-        for position in positions :
-            # stock_price = data_library.stock_price_library[position]
-            stock_price = data_library.read_csv(position, location)
-            # stock_price = pd.read_csv('')
-            try:
-                closing_price = stock_price.loc[stock_price.date_time == date_time, 'close'].values[0]
-            except:
-                error_message = f"{position} does not have last price at {date_time}"
-                # print(error_message)
-                closing_price = positions[position]['last_price']
-                if closing_price is None:
-                    closing_price = positions[position]['price']
-                self.error_manager.add_error_message(position, self.update_account_summary.__name__, error_message)
-
-            positions[position]['last_price'] = closing_price
-            positions[position]['market_value'] = closing_price * positions[position]['shares']
-            positions[position]['unrealized_pnl'] = positions[position]['market_value'] - positions[position]['total_cost']
-            net_asset_value = net_asset_value + positions[position]['market_value']
+        portfolio_threading = Portfolio_Multithread(position_dict=positions, date_time=date_time, data_library=data_library,
+                                                    error_manager=self.error_manager)
+        net_asset_value = portfolio_threading.run(positions)
 
         self.net_asset_value = net_asset_value + self.cash
         self.account_summary.add_account_summary(date_time, self.net_asset_value, self.cash)
-        self.account_summary.add_position_history(date_time, copy.deepcopy(positions))
-        self.positions = positions
+        self.account_summary.add_position_history(date_time, copy.deepcopy(portfolio_threading.positions))
+        self.positions = portfolio_threading.positions
+
 
 
     def monitor_sell_cond(self, sell_cond_func, sell_cond_params):
@@ -96,15 +78,42 @@ class Portfolio(Account) :
             if sell_cond_func(**sell_cond_params) :
                 self.positions_pending_to_sell.append(self.positions[position])
 
+
+
+
+
+
     
-
-
-
-
     
-    
+class Portfolio_Multithread :
+    def __init__(self, position_dict, date_time, data_library, error_manager):
+        self.positions = position_dict
+        self.date_time = date_time
+        self.data_library = data_library
+        self.error_manager = error_manager
 
+    def update_position_value(self, position, location='US'):
 
+        stock_price = self.data_library.read_csv(position, location)
+        try:
+            closing_price = stock_price.loc[stock_price.date_time == self.date_time, 'close'].values[0]
+        except:
+            error_message = f"{position} does not have last price at {self.date_time}"
+            # print(error_message)
+            closing_price = self.positions[position]['last_price']
+            if closing_price is None:
+                closing_price = self.positions[position]['price']
+            self.error_manager.add_error_message(position, "update_account_summary", error_message)
 
+        self.positions[position]['last_price'] = closing_price
+        self.positions[position]['market_value'] = closing_price * self.positions[position]['shares']
+        self.positions[position]['unrealized_pnl'] = self.positions[position]['market_value'] - self.positions[position]['total_cost']
+        return self.positions[position]['market_value']
+
+    def run(self, positions):
+        t = ThreadPool(processes=8)
+        market_cap_value_list = t.map(self.update_position_value, list(positions.keys()))
+        t.close()
+        return sum(market_cap_value_list)
 
 
